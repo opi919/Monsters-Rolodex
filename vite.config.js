8570fa35119697d13c0961ce1425ab0be4bcc2f2
opi919 <khandokermomen919@gmail.com>
2023-09-18 23:42:29 +0600

    bug fixed

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist', // Ensure this points to your desired output directory
-  },
-  base: "/Monsters-Rolodex/",
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/Monsters-Rolodex/'
+  }
+
+  return config
 })
